dd231fffc7dffb63e06fa0fbf73f018ec7fca74c
Foster Gailey <foster@gaileyhome.com>
2019-10-29 23:41:58 -0400

    added framework for remaining functions, added log

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+// All Required files
 require("dotenv").config();
 let Spotify = require('node-spotify-api');
 let fs = require('fs')
@@ -11,25 +12,23 @@ let spotify = new Spotify(masterKey.spotify);
 
 // console.log(spotify);
 
-console.log(masterKey.omdb)
+// console.log(masterKey.omdb)
 
+//inputs
 let search = process.argv[2]
-
 let param = process.argv.slice(3).join(' ')
 
 
 if(!search){
     search = "concert-this"
 }
-if(!param){
-    param = "The Knocks"
-}
 console.log(search)
 console.log(param)
 
 
-
+// ================BEGIN FUNCTIONS============================
 let findConcert = function(param){
+    
     axios.get(`https://rest.bandsintown.com/artists/${param}/events?app_id=${masterKey.band}`)
     .then(function(response){
         for(let i = 0; i < 10; i++){
@@ -37,50 +36,74 @@ let findConcert = function(param){
             let venueName = info.venue.name
             let venueAddress = `${info.venue.city} ${info.venue.region}`
             let concertDate =  moment(info.datetime).format('L');
-            console.log(`
+            let venueLog = `
             ------------------------------
-                Venue Name: ${venueName}
-                Venue Address: ${venueAddress}
-                Venue Date: ${concertDate}
-            ===============================`)
+            Venue Name: ${venueName}
+            Venue Address: ${venueAddress}
+            Venue Date: ${concertDate}
+            ===============================
+            `
+
+            console.log(venueLog)
+
+            fs.appendFile("log.txt", `${search}, ${param}, ${venueLog}`, function(err){
+                if (err) {
+                    console.log(err);
+                  }
+                
+                  // If no error is experienced, we'll log the phrase "Content Added" to our node console.
+                //   else {
+                //     console.log("Content Added!");
+                //   }
+            })
         }
     })
 }
 
-// { offers:
-//     [ { type: 'Tickets',
-//         url:
-//          'https://www.bandsintown.com/t/1016784651?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=ticket',
-//         status: 'available' } ],
-//    venue:
-//     { name: 'Royal Oak Music Theatre',
-//       country: 'United States',
-//       region: 'MI',
-//       city: 'Royal Oak',
-//       latitude: '42.4873085',
-//       longitude: '-83.14743529999998' },
-//    datetime: '2019-10-31T19:00:00',
-//    on_sale_datetime: '2019-05-23T10:00:00',
-//    description: 'Gryffin: GRAVITY II TOUR with The Knocks (DJ Set) & Bunt',
-//    lineup: [ 'The Knocks' ],
-//    id: '1016784651',
-//    artist_id: '835569',
-//    url:
-//     'https://www.bandsintown.com/e/1016784651?app_id=codingbootcamp&came_from=267&utm_medium=api&utm_source=public_api&utm_campaign=event' }
-// findConcert()
+let findSong = function(param){
+    
+    spotify.search({ type: 'track', query: param }, function(err, data) {
+        console.log(param)
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+        console.log(data.items[1]); 
+    });
+};
 
+let findMovie = function(param){
+    console.log('movie')
+}
+
+let buckWild = function(param){
+    console.log('wild')
+}
+
+// ==================END FUNCTIONS============================
 switch (search){
     case 'concert-this':
-        console.log('working')
+        if(!param){
+            param = "The Knocks"
+        }
+        console.log('concert yup')
         findConcert(param);
     break;
     case 'spotify-this-song':
-        findSong()
+        if(!param){
+            param = 'Uneventful Days'
+        }
+        console.log('spotify yea')
+        findSong(param);
     break;
     case 'movie-this':
-        
+        if(!param){
+            param = 'Jaws'
+        }
+        console.log('a movie it be')
+        findMovie();
     break;
     case 'do-what-it-says':
-
+        console.log('wild west out in here')
+        buckWild();
     break;
 }
